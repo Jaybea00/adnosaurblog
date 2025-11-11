154e49108204aad53e4727a8d7cfa7b59cfbf1dc
Kolade1024 <akoladeemmanuel14@gmail.com>
2025-11-11 16:02:05 +0100

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,7 +59,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin="anonymous" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link rel="preconnect" href="https://www.youtube.com" crossOrigin="anonymous" />
-    
+        <link rel="preconnect" href="https://cdn.jsdelivr.net"/>
       </head>
       <body>
         <Header />
